7ca65d3ac1d97215cbba901e9c7a00a5a6761987
siner308 <siner308@gmail.com>
2020-08-25 01:50:16 +0900

    irk-event-worker configuration 수정

--- irk-event-worker/src/config/settings_sample.py
+++ irk-event-worker/src/config/settings_sample.py
@@ -3,9 +3,10 @@ import os
 # Server
 ENV = os.environ['ENV']
 SERVER_URL = 'http://127.0.0.1:5784'
-LOG_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../../logs'
-SCREENSHOT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../../screenshots'
-CHROMEDRIVER_PATH = os.path.dirname(os.path.realpath(__file__)) + '/' + '../../binaries/chromedriver'
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../../../'
+LOG_DIR = BASE_DIR + '/data/logs'
+SCREENSHOT_DIR = BASE_DIR + '/data/screenshots'
+CHROMEDRIVER_PATH = BASE_DIR + '/irk-event-worker/binaries/chromedriver'
 
 # Ingress
 GOOGLE_EMAIL = 'XXXXXXX'
@@ -15,7 +16,7 @@ GOOGLE_MAP_KEY = 'XXXXXXX'
 MAX_LOAD_TIME = 300
 
 # Redis
-REDIS_HOST = 'locahost'
+REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_PASSWORD = 'password'
 
