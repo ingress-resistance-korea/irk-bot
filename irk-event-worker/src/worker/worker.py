90bb5fa99c1476cc221c36bc9b53ccc700ec912c
siner308 <siner308@gmail.com>
2020-08-25 02:16:01 +0900

    [irk-event-worker] 키 변경 및 로그 추가

--- irk-event-worker/src/worker/worker.py
+++ irk-event-worker/src/worker/worker.py
@@ -14,13 +14,15 @@ class Worker:
         self.queue = Queue()
 
     def run(self):
-        self.queue.rpush(INTEL_REQUEST, json.dumps({"user": "SinerDJ", "value": "올림픽공원"}))  # sample test
+        self.queue.rpush(INTEL_REQUEST, json.dumps({"user": "SinerDJ", "search_key": "올림픽공원"}))  # sample test
         while True:
             request_str = self.queue.lpop(INTEL_REQUEST)
             if request_str:
                 request = json.loads(request_str)
-                search_key = request['value']
+                search_key = request['search_key']
                 is_success, text = get_intel_screenshot(self.chromedriver, search_key=search_key)
                 request['is_success'] = is_success
                 request['text'] = text
-                self.queue.rpush(INTEL_RESPONSE, json.dumps(request))
+                intel_response = json.dumps(request)
+                self.logger.info(intel_response)
+                self.queue.rpush(INTEL_RESPONSE, intel_response)
